9db9d14b63f21a27b14b9469f9951ab6c50fd34e
parkerdunbar <PDunbar@student.neumont.edu>
2018-05-16 11:03:40 -0600

    Fixed HTML Component Factory

--- Factory.project.pro2/src/main/java/curtis/max/csc360/HTMLComponentFactory.java
+++ Factory.project.pro2/src/main/java/curtis/max/csc360/HTMLComponentFactory.java
@@ -10,14 +10,13 @@ public class HTMLComponentFactory extends ComponentFactory {
 		if (type.equalsIgnoreCase("Button")) {
 			return new HTMLButton(contex, top, left, width, height);
 		} else if (type.equalsIgnoreCase("Label")) {
-			
+			return new HTMLLabel(contex, top, left, width, height);
 		} else if (type.equalsIgnoreCase("TextField")) {
-
+			return new HTMLTextField(contex, top, left, width, height);
 		} else if (type.equalsIgnoreCase("H1Tag")) {
-
+			return new HTMLH1Tag(contex, top, left, width, height);
 		} else {
 			throw new IllegalArgumentException(type + " is not a supported type");
 		}
-		return null;
 	}
 }
